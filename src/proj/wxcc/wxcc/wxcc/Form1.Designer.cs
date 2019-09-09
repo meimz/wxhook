92a07baf439b67e53cddad69c580c05e708f57dd
mrcong <zh-lei@foxmail.com>
2019-09-09 15:24:50 +0800

    新增通信项目

---
+++ src/proj/wxcc/wxcc/wxcc/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace wxcc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(478, 77);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(75, 23);
+            this.bt1.TabIndex = 0;
+            this.bt1.Text = "发送信息";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(319, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ src/proj/wxcc/wxcc/wxcc/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace wxcc
+{
+    public partial class Form1 : Form
+    {
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            WatchLog_Print(1, textBox1.Text);
+
+        }
+
+        // 消息打印
+        public void WatchLog_Print(int logType, string strs)
+        {
+            int mark = ImportFromDLL.WM_COPYDATA;
+            IntPtr wechatWindowHandler = ImportFromDLL.FindWindow("WeChatHelper",null);
+
+
+            IntPtr init = Marshal.StringToHGlobalAnsi(strs);
+            ImportFromDLL.COPYDATASTRUCT msg = new ImportFromDLL.COPYDATASTRUCT
+            {
+                dwData = 5,
+                cbData = Encoding.Default.GetBytes(strs).Length + 1, //长度 注意不要用strText.Length;  
+                lpData = init
+            };
+
+
+            var t = ImportFromDLL.SendMessage(wechatWindowHandler, mark, (IntPtr)logType, ref msg);
+        }
+
+    }
+}
